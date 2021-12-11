3e8a95063b2f0654c2c557fcc3a6cde722258e34
James Childress <james.childress@zapier.com>
2021-12-11 15:20:44 -0800

    more lyrics

--- song-data.js
+++ song-data.js
@@ -1,31 +1,63 @@
-const NEWLINE = "\n";
-const SPACE = " ";
+(function () {
+  var NEWLINE = "\n";
+  var SPACE = " ";
+  var PAUSE = "";
 
-window.songData = {
-  bpm: 120,
-  offset: 0.1,
-  lyrics: [
-    [2.5, NEWLINE],
-    [0.5, "ca"],
-    [0.5, "reer"],
-    [0, SPACE],
-    [0.5, "boy"],
-    [4, NEWLINE],
-    [0.5, "work"],
-    [0.5, ""],
-    [0.5, "ing"],
-    [0, SPACE],
-    [1, "o"],
-    [0.5, "ver"],
-    [2, "time"],
-    [5.5, NEWLINE],
-    [0.5, "ca"],
-    [0.5, "reer"],
-    [0, SPACE],
-    [0.5, "boy"],
-    [4, NEWLINE],
-    [0.5, "one"],
-    [0.5, " "],
-    [0.5, "thing"],
-  ],
-};
+  window.songData = {
+    bpm: 120,
+    offset: 0.1,
+    lyrics: [
+      [2.5, PAUSE],
+      [0.5, "ca"],
+      [0.5, "reer"],
+      [0, SPACE],
+      [0.5, "boy"],
+
+      [4, NEWLINE],
+      [0.5, "work"],
+      [0.5, PAUSE],
+      [0.5, "ing"],
+      [0, SPACE],
+      [1, "ove"],
+      [0.5, "r"],
+      [2, "time"],
+
+      [5.5, NEWLINE],
+      [0.5, "ca"],
+      [0.5, "reer"],
+      [0, SPACE],
+      [0.5, "boy"],
+
+      [4, NEWLINE],
+      [0.25, "one"],
+      [0.75, SPACE],
+      [0.25, "thing"],
+      [0.25, SPACE],
+      [0.25, "on"],
+      [0.5, SPACE],
+      [0.25, "my"],
+      [0.25, SPACE],
+      [1.75, "mind"],
+
+      [6, NEWLINE],
+      [0.25, "cheap"],
+      [0.25, SPACE],
+      [0.5, "office"],
+      [0.25, SPACE],
+      [0.75, "coffee"],
+
+      [3, NEWLINE],
+      [0.25, "is"],
+      [0.25, SPACE],
+      [0.25, "pump"],
+      [0.5, PAUSE],
+      [0.25, "ing"],
+      [0.25, SPACE],
+      [0.25, "through"],
+      [0.5, SPACE],
+      [0.25, "my"],
+      [0.25, SPACE],
+      [2, "veins"],
+    ],
+  };
+})();
--- video/script.js
+++ video/script.js
@@ -1,53 +1,57 @@
-var BEAT_DURATION = 60 / window.songData.bpm;
-var OFFSET = window.songData.offset;
-var TEXT_CLASS_NAME = "kara-text";
-var HIGHLIGHT_CLASS_NAME = "kara-text-highlight";
+(function () {
+  var TEXT_CLASS_NAME = "kara-text";
+  var HIGHLIGHT_CLASS_NAME = "kara-text-highlight";
+  var NEWLINE = "\n";
 
-var segmentContainer = document.getElementById("kara-segments-container");
+  var BEAT_DURATION = 60 / window.songData.bpm;
+  var OFFSET = window.songData.offset;
 
-function createSegmentNode(text) {
-  var container = document.createElement("div");
-  container.classList.add("kara-segment");
-  container.innerHTML =
-    "<span class='" +
-    TEXT_CLASS_NAME +
-    "'>" +
-    text +
-    "</span><span class='" +
-    HIGHLIGHT_CLASS_NAME +
-    "'>" +
-    text +
-    "</span>";
-  segmentContainer.appendChild(container);
-  return container;
-}
+  var segmentContainer = document.getElementById("kara-segments-container");
 
-function animateSegmentNode(node, percent) {
-  node.lastElementChild.style.setProperty("width", percent * 100 + "%");
-}
+  function createSegmentNode(text) {
+    var container = document.createElement("div");
+    container.classList.add("kara-segment");
+    container.innerHTML =
+      "<span class='" +
+      TEXT_CLASS_NAME +
+      "'>" +
+      text +
+      "</span><span class='" +
+      HIGHLIGHT_CLASS_NAME +
+      "'>" +
+      text +
+      "</span>";
+    segmentContainer.appendChild(container);
+    return container;
+  }
 
-var totalDuration = 0;
-var segments = [];
+  function animateSegmentNode(node, percent) {
+    node.lastElementChild.style.setProperty("width", percent * 100 + "%");
+  }
 
-window.songData.lyrics.forEach(function (lyric) {
-  const endTime = totalDuration + BEAT_DURATION * lyric[0];
-  segments.push({
-    start: totalDuration,
-    end: endTime,
-    node: createSegmentNode(lyric[1]),
+  var totalDuration = 0;
+  var segments = [];
+
+  window.songData.lyrics.forEach(function (lyric) {
+    const endTime = totalDuration + BEAT_DURATION * lyric[0];
+    segments.push({
+      start: totalDuration,
+      end: endTime,
+      node: createSegmentNode(lyric[1]),
+    });
+    totalDuration = endTime;
   });
-  totalDuration = endTime;
-});
 
-var runLoop = function (time, browser, currentFrame, frameRate) {
-  const currentSongTime = currentFrame / frameRate + OFFSET;
-  segments.forEach(function (segment, index) {
-    var duration = segment.end - segment.start;
-    var elapsed = currentSongTime - segment.start;
-    var percent = duration ? Math.max(0, Math.min(1, elapsed / duration)) : 0;
-    animateSegmentNode(segment.node, percent);
-  });
-};
+  var runLoop = function (time, browser, currentFrame, frameRate) {
+    const currentSongTime = currentFrame / frameRate + OFFSET;
+    segments.forEach(function (segment, index) {
+      var duration = segment.end - segment.start;
+      var elapsed = currentSongTime - segment.start;
+      var percent = duration ? Math.max(0, Math.min(1, elapsed / duration)) : 0;
+      animateSegmentNode(segment.node, percent);
+    });
+  };
 
-// This is where webvfx expects to find custom effect functions.
-window.webvfx_add_to_frame = [runLoop];
+  // This is where webvfx expects to find custom effect functions.
+  window.webvfx_add_to_frame = [runLoop];
+})();
